a055ff563559f6007d6f46431627f33416b5f565
CAM603 <cameronhawley@comcast.net>
2020-06-23 09:04:56 -0400

    first pass not solved

---
+++ add_two.py
@@ -0,0 +1,50 @@
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+def addTwoNumbers(l1, l2):
+    dic = {l1: [], l2: []}
+
+    # put node values in a stack
+    current = (l1, l2)
+    while current[0] is not None:
+        dic[l1].append(current[0].val)
+        dic[l2].append(current[1].val)
+        current = (current[0].next, current[1].next)
+
+    str_one = ""
+    str_two = ""
+
+    # pop from stack to unreverse number
+    while len(dic[l1]) > 0:
+        str_one += str(dic[l1].pop())
+    while len(dic[l2]) > 0:
+        str_two += str(dic[l2].pop())
+
+    num = int(str_one) + int(str_two)
+
+    num_list = [int(x) for x in str(num)]
+    num_list.reverse()
+
+    # create linked list representing new number
+    root = None
+    for i in range(len(num_list) - 1, -1, -1):
+        temp = ListNode(0)
+        temp.val = num_list[i]
+        temp.next = root
+        root = temp
+
+    return root
+
+
+c = ListNode(3)
+b = ListNode(4, c)
+a = ListNode(2, b)
+
+g = ListNode(4)
+f = ListNode(6, g)
+e = ListNode(5, f)
+
+addTwoNumbers(a, e)
